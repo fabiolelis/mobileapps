fde41ebfce49bf244a0f8788ee99f9d071ec6051
Fabio Lelis <fabiolelis@msn.com>
2016-04-03 05:56:56 +0100

    right timer for god sake

--- MobileAppsProject/MainPage.xaml.cs
+++ MobileAppsProject/MainPage.xaml.cs
@@ -20,6 +20,7 @@ using System.Diagnostics;
 using Windows.UI.Core;
 using System.ComponentModel;
 using System.Threading.Tasks;
+using Windows.System.Threading;
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace MobileAppsProject
@@ -32,18 +33,11 @@ namespace MobileAppsProject
 
         User user = null;
         Day _day = null;
-
-        private BackgroundWorker bw = new BackgroundWorker();
-
+        DispatcherTimer myStopwatchTimer;
 
         public MainPage()
         {
             this.InitializeComponent();
-
-            /*background works*/
-            bw.WorkerReportsProgress = true;
-            bw.DoWork += new DoWorkEventHandler(bw_DoWork);
-            bw.ProgressChanged += new ProgressChangedEventHandler(bw_ProgressChanged);
             
             var currentView = SystemNavigationManager.GetForCurrentView();
             currentView.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
@@ -59,68 +53,44 @@ namespace MobileAppsProject
                 int dayID = (int)localSettings.Values["dayID"];
                 _day = DayDB.getByDayID(dayID);
             }
-
-
-            if (bw.IsBusy != true)
-            {
-                bw.RunWorkerAsync();
-            }
+            
 
             if (user == null)
             {
-                Frame.Navigate(typeof(UserEdit));
+                user = new User();
+                helloUser.Text = "Go to menu and edit you info ->";
 
             }
 
-            
-             if (user != null)
+
+            if (user != null)
              {
                  helloUser.Text = "Hello, " + user.Name +"!";
                //  this.UserEditBtn.Content = "Edit user";
              }
-             /*
-             else
-             {
-                 helloUser.Text = "No user";
-                 this.UserEditBtn.Content = "Add user";
-             }
-             */
-        }
+            
 
-        private void bw_DoWork(object sender, DoWorkEventArgs e)
-        {
-            BackgroundWorker worker = sender as BackgroundWorker;
-
-            int i = 0, y = 0;
-            while(true)
-            {
-                
-                // Perform a time consuming operation and report progress.
-                // System.Threading.Thread.Sleep(500);
-                //Thread.Sleep(1000);
-
-                
-                i++;
-                y = i % 100;
-                worker.ReportProgress(y);
-            }
+            myStopwatchTimer = new DispatcherTimer();
+            myStopwatchTimer.Tick += changeUI;
+            myStopwatchTimer.Interval = new TimeSpan(0, 0, 0, 10, 0); // 1 second
+            myStopwatchTimer.Start();
+            changeUI(null, null);
 
         }
 
-        private void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        private void changeUI(object sender, object e)
         {
-
             //Get the closest future meal
 
-           
+
             if (this._day == null || (this._day.Date.Date.Year != DateTime.Now.Date.Year || this._day.Date.Date.DayOfYear != DateTime.Now.Date.DayOfYear))
             {
                 //create day
-                
+
                 this._day = new Day();
                 this._day.User = this.user;
                 this._day.Date = DateTime.Now;
-                this._day.LastMeal = (int) DateTime.Now.TimeOfDay.TotalMinutes;
+                this._day.LastMeal = (int)DateTime.Now.TimeOfDay.TotalMinutes;
                 DayDB ddb = new DayDB(this._day);
 
                 ddb.save();
@@ -130,7 +100,7 @@ namespace MobileAppsProject
 
             }
 
-            if(DateTime.Now.TimeOfDay.TotalMinutes > user.DinnerTime)
+            if (DateTime.Now.TimeOfDay.TotalMinutes > user.DinnerTime)
             {
                 this.countdown.Text = "no more food today...";
                 this.rectTime.Width = 0;
@@ -139,11 +109,10 @@ namespace MobileAppsProject
             {
                 TimeSpan until = untilNext();
 
-                //Task.Delay(TimeSpan.FromSeconds(1));
                 this.countdown.Text = until.Hours.ToString() + " hours and " + until.Minutes.ToString() + " minutes";
-                this.rectTime.Width = 200 * DateTime.Now.TimeOfDay.TotalMinutes/((double)until.TotalMinutes - (double)_day.LastMeal);
+                // fix it:
+                //this.rectTime.Width = 200 * ((DateTime.Now.TimeOfDay.TotalMinutes - (double)_day.LastMeal) - (double)until.TotalMinutes));
             }
-            
         }
 
         public TimeSpan untilNext()
@@ -216,11 +185,6 @@ namespace MobileAppsProject
             // free up system resources (the underlying socket and memory used for the object)
             httpClient.Dispose();
             */
-
-        }
-
-        private void imgSettings_Tapped(object sender, TappedRoutedEventArgs e)
-        {
 
         }
 
